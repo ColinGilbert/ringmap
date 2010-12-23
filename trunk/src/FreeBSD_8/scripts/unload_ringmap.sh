d62b6d18b4467c8cd330356db2bebdc6119c8dbc
pebu3op <pebu3op@e3d2752c-974e-11de-9e98-d1f6c90d23ca>
2010-12-23 16:36:41 +0000

    fixes + doc for stable

---
+++ trunk/src/FreeBSD_8/scripts/unload_ringmap.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+LIBPCAP_BUILD_DIR=/usr/src/lib/libpcap/
+
+check_module() {
+	kldstat | grep ringmap >/dev/null
+	if [ $? -eq 0 ]
+	then 
+		echo The ringmap module is loaded.
+		echo It will be first unloaded... && sleep 1
+		kldunload if_ringmap.ko
+		sleep 1
+	fi 
+}
+
+install_libpcap() {
+	cd ${LIBPCAP_BUILD_DIR}
+	make clean 
+	make || { echo "can not build libpcap" ; return 1 ; }
+	make install || { echo "Error by compiling driver" ; return 1 ; }
+	cd -
+}
+
+echo
+echo "===> Check the module"
+check_module || { Error! ; exit 1 ; }
+
+echo
+echo "===> Install libpcap:" && sleep 1
+echo
+install_libpcap || { echo "Error by installing libpcap" ; exit 1 ; }
+echo
+
+echo "If you want to use network interface with generic driver do kldload <module name>"
+echo
+
+exit 0;
